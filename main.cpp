938b94287c5cc681c6d3147c069b2762e8ab87cc
RaKalik963 <andrejmahot646@gmail.com>
2024-05-01 20:19:40 +0300

    lab-ex1

--- main.cpp
+++ main.cpp
@@ -4,7 +4,19 @@ using namespace std;
 
 int main()
 {
+	cout << "enter num ";
+	int num;
+	cin >> num;
 
+
+	if (num % 2 == 0)
+	{
+		cout << "your number is even" << endl;
+	}
+	else
+	{
+		cout << "your number is not even" << endl;
+	}
 	return 0;
 	system("pause");
 }
