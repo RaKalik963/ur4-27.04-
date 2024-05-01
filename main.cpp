8f94bf9daf3c634ad649219ffd95df3736746ed1
RaKalik963 <andrejmahot646@gmail.com>
2024-05-01 20:32:01 +0300

    lab-ex4

--- main.cpp
+++ main.cpp
@@ -4,7 +4,24 @@ using namespace std;
 
 int main()
 {
-
+	cout << "enter num1 ";
+	int x;
+	cin >> x;
+	cout << "enter num2 ";
+	int y;
+	cin >> y;
+	if (x > y)
+	{
+		cout << y << ", " << x << endl;
+	}
+	else if (x == y)
+	{
+		cout << "x = y" << endl;
+	}
+	else
+	{
+		cout << x << ", " << y << endl;
+	}
 	return 0;
 	system("pause");
 }
