4e4d1bee40b3434c954f82c6bd6ab0d43c672d9b
RaKalik963 <andrejmahot646@gmail.com>
2024-05-01 22:09:54 +0300

    dz-ex2

--- main.cpp
+++ main.cpp
@@ -4,7 +4,15 @@ using namespace std;
 
 int main()
 {
+    int number;
+    cout << "enter 4-digit number ";
+    cin >> number;
 
+    if (number < 1000 || number > 9999) {
+        cout << "your number are not 4-digit" << endl;
+    }
+    int changed_num = (number % 100) * 100 + (number / 100 % 10) * 10 + (number / 1000) * 1000 + (number / 10 % 10);
+    cout << "number after change " << changed_num << endl;
 	return 0;
 	system("pause");
 }
