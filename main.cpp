3181bc9ef961c6246c4d4aa24e306ac1895d179a
RaKalik963 <andrejmahot646@gmail.com>
2024-05-01 20:29:56 +0300

    lab-ex3

--- main.cpp
+++ main.cpp
@@ -4,7 +4,21 @@ using namespace std;
 
 int main()
 {
-
+	cout << "enter num ";
+	int num;
+	cin >> num;
+	if (num < 0)
+	{
+		cout << "your number negative" << endl;
+	}
+	else if (num == 0)
+	{
+		cout << "your number = 0" << endl;
+	}
+	else
+	{
+		cout << "your number positive" << endl;
+	}
 	return 0;
 	system("pause");
 }
